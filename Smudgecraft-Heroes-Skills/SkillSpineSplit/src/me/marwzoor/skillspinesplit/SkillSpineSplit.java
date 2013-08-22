91e6d101807bd51db6eb1d49570060ceebb473a6
Marwzoor <Marwzoor@Smudgecraft.net>
2013-08-22 20:38:34 +0200

    Import ItemStack - remember to never edit directly on github again

--- Smudgecraft-Heroes-Skills/SkillSpineSplit/src/me/marwzoor/skillspinesplit/SkillSpineSplit.java
+++ Smudgecraft-Heroes-Skills/SkillSpineSplit/src/me/marwzoor/skillspinesplit/SkillSpineSplit.java
@@ -7,6 +7,7 @@ import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.inventory.ItemStack;
 
 import com.herocraftonline.heroes.Heroes;
 import com.herocraftonline.heroes.api.events.WeaponDamageEvent;
