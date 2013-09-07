f648bc7fb81bd56f8853ed7a4cfc72d4f6dda1e6
Mio Engel <mioengel@hotmail.com>
2013-09-07 17:23:20 +0200

    Added Skill TwinLash

--- Smudgecraft-Heroes-Skills/SkillTwinLash/src/me/marwzoor/skilltwinlash/SkillTwinLash.java
+++ Smudgecraft-Heroes-Skills/SkillTwinLash/src/me/marwzoor/skilltwinlash/SkillTwinLash.java
@@ -1,17 +1,17 @@
-package me.marwzoor.skilldarkwhip;
+package me.marwzoor.skilltwinlash;
 
+import net.smudgecraft.heroeslib.HeroesLib;
 import net.smudgecraft.heroeslib.whip.WhipDamageEvent;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.entity.Damageable;
+import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
-import org.bukkit.potion.PotionEffect;
-import org.bukkit.potion.PotionEffectType;
 
 import com.herocraftonline.heroes.Heroes;
 import com.herocraftonline.heroes.api.SkillResult;
@@ -25,20 +25,20 @@ import com.herocraftonline.heroes.characters.skill.SkillSetting;
 import com.herocraftonline.heroes.characters.skill.SkillType;
 import com.herocraftonline.heroes.util.Messaging;
 
-public class SkillDarkWhip extends ActiveSkill
+public class SkillTwinLash extends ActiveSkill
 {
-	public static SkillDarkWhip skill;	
+	public static SkillTwinLash skill;	
 	
-	public SkillDarkWhip(Heroes instance)
+	public SkillTwinLash(Heroes instance)
 	{
-		super(instance, "DarkWhip");
+		super(instance, "TwinLash");
 		skill=this;
-		setDescription("Your next fling with your whip instantly kills a monster, or deals $X damage and cause confusion for $Y seconds to an enemy. M: $1 CD: $2");
-		setUsage("/skill darkwhip");
+		setDescription("Your lash with your whip will hit your enemy twice, dealing $X for every lash. M: $1 CD: $2");
+		setUsage("/skill twinlash");
 		setArgumentRange(0,0);
-		setIdentifiers(new String[] { "skill darkwhip" });
+		setIdentifiers(new String[] { "skill twinlash" });
 		setTypes(new SkillType[] { SkillType.BUFF });
-		Bukkit.getPluginManager().registerEvents(new DarkWhipLinstener(), plugin);
+		Bukkit.getPluginManager().registerEvents(new TwinLashLinstener(), plugin);
 	}
 	
 	public String getDescription(Hero hero)
@@ -47,12 +47,9 @@ public class SkillDarkWhip extends ActiveSkill
 		{
 			double damage = (SkillConfigManager.getUseSetting(hero, skill, SkillSetting.DAMAGE, 60, false) +
 					(SkillConfigManager.getUseSetting(hero, skill, SkillSetting.DAMAGE_INCREASE, 1, false) * hero.getSkillLevel(skill)));
-			double duration = (SkillConfigManager.getUseSetting(hero, skill, "confusion-duration", 5000, false) +
-					(SkillConfigManager.getUseSetting(hero, skill, "confusion-duration-increase", 1, false) * hero.getSkillLevel(skill)));
-			duration = duration / 1000;
 			int mana = SkillConfigManager.getUseSetting(hero, skill, SkillSetting.MANA, 70, false);
 			int cd = SkillConfigManager.getUseSetting(hero, skill, SkillSetting.COOLDOWN, 70, false);
-			return super.getDescription().replace("$X", damage + "").replace("$Y", duration + "").replace("$1", mana + "").replace("$2", cd + "");
+			return super.getDescription().replace("$X", damage + "").replace("$1", mana + "").replace("$2", cd + "");
 		}
 		else
 		{
@@ -65,8 +62,6 @@ public class SkillDarkWhip extends ActiveSkill
 		ConfigurationSection node = super.getDefaultConfig();
 		node.set(SkillSetting.DAMAGE.node(), 60);
 		node.set(SkillSetting.DAMAGE_INCREASE.node(), 1);
-		node.set("confusion-duration", 5000);
-		node.set("confusion-duration-increase", 1);
 		return node;
 	}
 	
@@ -75,28 +70,24 @@ public class SkillDarkWhip extends ActiveSkill
 	{
 		double damage = (SkillConfigManager.getUseSetting(hero, skill, SkillSetting.DAMAGE, 60, false) +
 				(SkillConfigManager.getUseSetting(hero, skill, SkillSetting.DAMAGE_INCREASE, 1, false) * hero.getSkillLevel(skill)));
-		double duration = (SkillConfigManager.getUseSetting(hero, skill, "confusion-duration", 5000, false) +
-				(SkillConfigManager.getUseSetting(hero, skill, "confusion-duration-increase", 1, false) * hero.getSkillLevel(skill)));
-		hero.addEffect(new DarkWhipEffect(this, duration, damage));
+		hero.addEffect(new TwinLashEffect(this, damage));
 		return SkillResult.NORMAL;
 	}
 	
-	public class DarkWhipEffect extends Effect
+	public class TwinLashEffect extends Effect
 	{
-		public final PotionEffect pe;
 		public final double damage;
 		
-		public DarkWhipEffect(Skill skill, double duration, double damage)
+		public TwinLashEffect(Skill skill, double damage)
 		{
-			super(skill, "DarkWhip");
-			pe = new PotionEffect(PotionEffectType.CONFUSION, (int) (duration/1000*20), 0);
+			super(skill, "TwinLash");
 			this.damage = damage;
 		}
 		
 		@Override
 		public void applyToHero(Hero hero)
 		{
-			Messaging.send(hero.getPlayer(), "Your next attack with your whip will fling darkness at your target!");
+			Messaging.send(hero.getPlayer(), "Your next attack with your whip will be a Twin Lash!");
 			super.applyToHero(hero);
 		}
 		
@@ -109,35 +100,64 @@ public class SkillDarkWhip extends ActiveSkill
 		@Override
 		public void removeFromHero(Hero hero)
 		{
-			Messaging.send(hero.getPlayer(), "Your will no longer fling darkness with your next attack!");
+			Messaging.send(hero.getPlayer(), "Your next attack will no longer be a Twin Lash!");
 			super.removeFromHero(hero);
 		}
 	}
 	
-	public class DarkWhipLinstener implements Listener
+	public class TwinLashLinstener implements Listener
 	{
 		@EventHandler(priority = EventPriority.HIGH)
-		public void onWhipDamageEvent(WhipDamageEvent event)
+		public void onWhipDamageEvent(final WhipDamageEvent event)
 		{
-			Hero hero = plugin.getCharacterManager().getHero(event.getAttacker());
-			if(!hero.hasEffect("DarkWhip"))
+			final Hero hero = plugin.getCharacterManager().getHero(event.getAttacker());
+			if(!hero.hasEffect("TwinLash"))
 				return;
-			DarkWhipEffect dwe = (DarkWhipEffect)hero.getEffect("DarkWhip");
+			event.setCancelled(true);
+			final TwinLashEffect twe = (TwinLashEffect)hero.getEffect("TwinLash");
 			if(event.getTarget() instanceof Player)
 			{
-				event.getTarget().addPotionEffect(dwe.pe);
-				event.setDamage(dwe.damage);
+				((LivingEntity)event.getTarget()).setLeashHolder(hero.getPlayer());
+				damageEntity((LivingEntity)event.getTarget(), hero.getPlayer(), twe.damage);
+				Bukkit.getScheduler().scheduleSyncDelayedTask(HeroesLib.plugin, new Runnable()
+				{
+					@Override
+					public void run()
+					{
+						((LivingEntity)event.getTarget()).setLeashHolder(null);
+						Bukkit.getScheduler().scheduleSyncDelayedTask(HeroesLib.plugin, new Runnable()
+						{
+							@Override
+							public void run()
+							{
+								((LivingEntity)event.getTarget()).setLeashHolder(hero.getPlayer());
+								damageEntity((LivingEntity)event.getTarget(), hero.getPlayer(), twe.damage);
+								Bukkit.getScheduler().scheduleSyncDelayedTask(HeroesLib.plugin, new Runnable()
+								{
+									@Override
+									public void run()
+									{
+										((LivingEntity)event.getTarget()).setLeashHolder(null);
+									}
+								},5L);
+							}
+						},1L);
+						
+					}
+				},5L);
 				skill.broadcast(event.getTarget().getLocation(), ChatColor.GRAY + "[" + ChatColor.DARK_RED + "Skill" + ChatColor.GRAY + "] " + ChatColor.DARK_RED + hero.getName() +
-						ChatColor.GRAY + " flings darkness with his whip at " + ChatColor.DARK_RED + ((Player)event.getTarget()).getName() + ChatColor.GRAY + "!");
+						ChatColor.GRAY + " performed a " + ChatColor.WHITE + "TwinLash" + ChatColor.GRAY + " on " + ChatColor.DARK_RED + 
+						((Player)event.getTarget()).getName() + ChatColor.GRAY + "!");
 			}
 			else
 			{
 				event.setDamage(((Damageable)event.getTarget()).getHealth());
 				skill.broadcast(event.getTarget().getLocation(), ChatColor.GRAY + "[" + ChatColor.DARK_RED + "Skill" + ChatColor.GRAY + "] " + ChatColor.DARK_RED + hero.getName() +
-						ChatColor.GRAY + " flings darkness with his whip at " + ChatColor.DARK_GREEN + event.getTarget().getType().getName() + ChatColor.GRAY + "!");
+						ChatColor.GRAY + " performed a " + ChatColor.WHITE + "TwinLash" + ChatColor.GRAY + " on " + 
+						ChatColor.DARK_GREEN + event.getTarget().getType().getName() + ChatColor.GRAY + "!");
 			}
 			
-			hero.removeEffect(dwe);
+			hero.removeEffect(twe);
 		}
 	}
 }
